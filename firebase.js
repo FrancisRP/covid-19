7432a52607473e14b81a38243e5334e4811f1378
Francis Pegenia <francispegenia15@gmail.com>
2020-09-28 21:42:52 +0800

    Adding the dropdown for the countries

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBxkb8tYxMHYf0QszTsL_t7StLppPCLBgk",
+    authDomain: "covid-19-89122.firebaseapp.com",
+    databaseURL: "https://covid-19-89122.firebaseio.com",
+    projectId: "covid-19-89122",
+    storageBucket: "covid-19-89122.appspot.com",
+    messagingSenderId: "931676033111",
+    appId: "1:931676033111:web:80deea38f61cdf9e79d312"
+  };
